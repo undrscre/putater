ac29c60db8ab220ae29d49b85cad1cea1b005fea
CelestialCrafter <celestialcraftermc@gmail.com>
2025-03-17 11:02:39 -0500

    change devices/memory to use stdlib::io things

--- src/memory.rs
+++ src/memory.rs
@@ -1,68 +1,102 @@
+use std::{fmt, io};
+
 use crate::devices;
 use crate::devices::Device;
-use log::warn;
+
+pub enum MemoryBusError {
+    NoDeviceOnPage(u8),
+}
+
+impl fmt::Display for MemoryBusError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            MemoryBusError::NoDeviceOnPage(page) => write!(f, "no device loaded on page {}", page),
+        }
+    }
+}
 
 pub struct MemoryBus {
-    ram: [[u8; 256]; 251], // 250x256 = 64000 usable bytes of memory
+    pub page: u8,
+    ram: [io::Cursor<[u8; 256]>; 251], // 250x256 = 64000 usable bytes of memory
     devices: [Option<Box<dyn Device>>; 5],
 }
 
 impl MemoryBus {
     pub fn new() -> Self {
         Self {
-            ram: [[0; 256]; 251],
-            devices: [None, None, None, None, None] // start out with none loaded
+            page: 0,
+            ram: [const { io::Cursor::new([0; 256]) }; 251],
+            devices: [None, None, None, None, None], // start out with none loaded
         }
     }
 
-    pub fn attach_device(&mut self, device: Box<dyn Device>) -> Result<(), ()> {
-        if let Some(slot) = self.devices.iter_mut().position(|d| d.is_none()) {
-            self.devices[slot] = Some(device);
-			println!("attached device!");
-            Ok(())
-        } else {
-            Err(())
-        }
+    pub fn attach_device(&mut self, device: Box<dyn Device>) -> Option<()> {
+        let _ = self.devices
+            .iter_mut()
+            .find(|d| d.is_none())?
+            .insert(device);
+        Some(())
     }
+}
 
-    pub fn read(&mut self, page: u8, offset: u8) -> u8 {
-        match page {
+impl io::Read for MemoryBus {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        match self.page {
+            // device pages
             251..=255 => {
-                let idx = (page - 251) as usize;
-                if let Some(device) = &self.devices[idx] {
-                    device.read(offset)
+                let idx = (self.page - 251) as usize;
+                if let Some(ref mut device) = &mut self.devices[idx] {
+                    device.read(buf)
                 } else {
-                    warn!("no device loaded on page {page}!");
-                    0
+                    Err(io::ErrorKind::NotFound.into())
                 }
-            },
-            _ => self.ram[page as usize][offset as usize],
+            }
+            _ => self.ram[self.page as usize].read(buf),
         }
     }
+}
 
-    pub fn write(&mut self, page: u8, offset: u8, value: u8) {
-        match page {
+impl io::Seek for MemoryBus {
+    fn seek(&mut self, pos: io::SeekFrom) -> io::Result<u64> {
+        if self.page >= 250 {
+            return Err(io::ErrorKind::NotSeekable.into())
+        }
+
+        self.ram[self.page as usize].seek(pos)
+    }
+}
+
+impl io::Write for MemoryBus {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        match self.page {
             250 => {
-                match offset {
-                    0 => {
-                        let device: Box<dyn Device> = match value {
-                            1 => Box::new(devices::TestDevice),
-                            _ => Box::new(devices::NullDevice),
-                        };
-						self.attach_device(device).unwrap();
-                    },
-                    _ => {}
-                }
-            },
+                // @TODO offload device initialization somewhere else, it doesnt make sense in the memory bus
+                Ok(buf.into_iter().try_fold(0, |acc, id| {
+                    let device: Box<dyn Device> = match id {
+                        0 => Box::new(devices::TestDevice::new()),
+                        1 => Box::new(devices::NullDevice::new()),
+                        _ => return Err(io::ErrorKind::NotFound.into()),
+                    };
+
+                    self.attach_device(device)
+                        .ok_or(io::ErrorKind::QuotaExceeded)
+                        .map(|_| acc + 1)
+                })?)
+            }
             251..=255 => {
-                let idx = (page - 251) as usize;
+                let idx = (self.page - 251) as usize;
                 if let Some(device) = &mut self.devices[idx] {
-                    device.write(offset, page)
+                    device.write(buf)
                 } else {
-                    warn!("no device loaded on page {page}!");
+                    Err(io::ErrorKind::NotFound.into())
                 }
-            },
-            _ => self.ram[page as usize][offset as usize] = value
+            }
+            _ => self.ram[self.page as usize].write(buf),
         }
+    }
+
+    // MemoryBus does not impl any buffering
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
     }
 }
